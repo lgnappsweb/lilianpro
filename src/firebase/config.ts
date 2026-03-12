73a8228854426f7326d152a78f46e59240ca0d41
Apps Web <lgnappsweb@gmail.com>
2026-03-12 13:47:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8093714598-5f213",
+  "appId": "1:697977138540:web:53b6321bf5b517dc2cd749",
+  "apiKey": "AIzaSyDuKYifdQVS6iP9OPFcp3kpJL6Hk-ybTY0",
+  "authDomain": "studio-8093714598-5f213.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "697977138540"
+};
